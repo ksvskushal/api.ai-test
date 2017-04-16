20998c450160c163bfc12ca41018f2de6c6827b5
Kushal K S V S <kkushal32@gmail.com>
2017-04-16 06:59:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "class.assignment":
+    if req.get("result").get("action") != "class-assignment":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
