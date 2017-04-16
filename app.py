8255125b10ae9535b014bc909a70a05d7dadf3e1
Kushal K S V S <kkushal32@gmail.com>
2017-04-16 09:58:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,58 +28,30 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") == "class-assignment":
-      	result = req.get("result")
-      	parameters = result.get("parameters")
-      	zone = parameters.get("subjects")
+    if req.get("result").get("action") != "class-assignment":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("subjects")
 
-      	cost = {'FLAT':'20th April', 'Software':'5th May', 'Algorithms':1, 'COA':1}
-	
-      	if cost[zone] != 1:
-        	speech = "The deadline for " + zone + " assignment is " + str(cost[zone]) + "."
-      	if cost[zone] == 1:
-        	speech = "The assignment for " + zone + " is done""."
-
-      	print("Response:")
-      	print(speech)
-
-      	return {
-          	"speech": speech,
-          	"displayText": speech,
-          	#"data": {},
-          	# "contextOut": [],
-          	"source": "apiai-onlinestore-shipping"
-      	}
-        
-    elif req.get("result").get("action") == "class.timings":
-      	result = req.get("result")
-      	parameters = result.get("parameters")
-      	zone = parameters.get("timings-relative")
-
-      	cost = {'today': 1 , 'tomorrow': 2}
-      	calendarfortoday = {"Algorithms" : "10 AM" , "COA" : "11 AM", "Software" : "9 AM"}
-	    calendarfortomorrow = {"Algorithms" : "9 AM" , "COA" : "10 AM", "Software" : "11 AM"}
+    cost = {'FLAT':'20th April', 'Software':'5th May', 'Algorithms':1, 'COA':1}
     
-      	if cost[zone] == 1:
-        	speech = "The timetable for " + zone + " is " + str(calendarfortoday["Software"]) + "\n" + 
-        												str(calendarfortoday["Algorithms"]) + "\n" +
-        												str(calendarfortoday["COA"]) + " . "
-      	if cost[zone] != 1:
-         	speech = "The timetable for " + zone + " is " + str(calendarfortomorrow["algorithms"]) + "\n" + 
-        												str(calendarfortomorrow["COA"]) + "\n" +
-        												str(calendarfortomorrow["Software"]) + " . "
-      	print("Response:")
-      	print(speech)
+    if cost[zone] != 1:
+        speech = "The deadline for " + zone + " assignment is " + str(cost[zone]) + "."
+    if cost[zone] == 1:
+        speech = "The assignment for " + zone + " is done""."
 
-      	return {
-          	"speech": speech,
-          	"displayText": speech,
-          	#"data": {},
-          	# "contextOut": [],
-          	"source": "apiai-onlinestore-shipping"
-        }
-        
-    else: return {}
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-shipping"
+    }
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
